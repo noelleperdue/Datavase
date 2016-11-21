de72f3844825e29c5e050849724a1fc13d621719
Noelle <perdue.noelle@gmail.com>
2016-11-21 17:20:20 +0000

    third draft front end to unify

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 app.set("view engine", "ejs");
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+app.get("/plant", (req, res) =>  {
+  res.render("plantPage")
+})
 
 app.listen(PORT, () => {
   console.log("App listening on port " + PORT);
