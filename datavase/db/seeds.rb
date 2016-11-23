c331e61c38212f3de62f70acc3f01a39033bd718
Noelle <perdue.noelle@gmail.com>
2016-11-23 16:41:54 +0000

    dropdown js fixed

--- datavase/db/seeds.rb
+++ datavase/db/seeds.rb
@@ -8,7 +8,7 @@
 require 'csv'
 
 # Plant.destroy_all
-#
+
 # csv_text = File.read(Rails.root.join('lib', 'seeds', 'species.csv'))
 # csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 # csv.each do |row|
